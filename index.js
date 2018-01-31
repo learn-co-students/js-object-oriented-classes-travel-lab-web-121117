49d6eab4077228ae898a158eb15526135c095628
markbello <mcbello51286@gmail.com>
2018-01-31 13:19:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+class Driver {
+  constructor(name, joinDate){
+    this.name = name
+    this.joinDate = joinDate
+    this.startDate = new Date(this.joinDate)
+    }
 
+  yearsExperienceFromBeginningOf(year){
+    const beginningYear = this.startDate.getFullYear()
+    return year - beginningYear
+  }
+
+}
+
+class Route {
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation
+    this.endingLocation = endingLocation
+  }
+
+  blocksTravelled(){
+    const eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    const nsBlocks = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    const ewBlocks = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+    return nsBlocks + ewBlocks
+  }
+
+  estimatedTime(peak){
+    if(peak){
+      return this.blocksTravelled() / 2
+    } else{
+      return this.blocksTravelled() / 3
+    }
+  }
+}
